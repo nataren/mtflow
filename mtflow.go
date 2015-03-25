cb3c6b2714c6d343e9314ee0a5c0b19539ddaa07
César López-Natarén <cesarn@mindtouch.com>
2015-03-25 07:22:48 -0700

    Use raw event sources

--- mtflow.go
+++ mtflow.go
@@ -1,34 +1,58 @@
 package main
 
-import(
-  "github.com/wm/go-flowdock/flowdock"
-  "code.google.com/p/goauth2/oauth"
-  "os"
-  "log"
+import (
+	"log"
+	"os"
+	"flag"
+	"fmt"
+	"github.com/bernerdschaefer/eventsource"
+	"time"
+	"net/http"
 )
 
 const (
-  FLOWDOCK_API_TOKEN = "FLOWDOCK_API_TOKEN"
+	FLOWDOCK_API_TOKEN = "FLOWDOCK_API_TOKEN"
 )
 
 func main() {
 
-  // Config
-  accessToken := os.Getenv(FLOWDOCK_API_TOKEN)
-  if accessToken == "" {
-    log.Fatalf("%s environment variable not found", FLOWDOCK_API_TOKEN)
-  }
+	// Environment variables definitions
+	accessToken := os.Getenv(FLOWDOCK_API_TOKEN)
 
-  // Auth
-  t := &oauth.Transport{
-    Token: &oauth.Token { AccessToken: accessToken },
-  }
-  client := flowdock.NewClient(t.Client())
+	// Command-line arguments definition
+	var organization string
+	flag.StringVar(&organization, "organization", "", "The organization name")
+	var flow string
+	flag.StringVar(&flow, "flow", "", "The flow to stream from")
+	flag.Parse()
 
-  // list all flows the authenticated user is a member of or can join
-  flows, _, err := client.Flows.List(true, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  log.Println(flows)
+	// Validation
+	if accessToken == "" {
+		log.Fatalf("%s environment variable not found", FLOWDOCK_API_TOKEN)
+	}
+	if organization == "" {
+		log.Fatal("'organization' is a required parameter")
+	}
+
+	// Build the HTTP request
+	streamURL := fmt.Sprintf("https://%v@stream.flowdock.com/flows/%v/%v", accessToken, organization, flow)
+    request, err := http.NewRequest("GET", streamURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header = map[string][]string {
+        "Accept": {"text/event-stream"},
+        "Content-Type": {"text/event-stream"},
+    }
+
+	// Build the event source
+	source := eventsource.New(request, 3 * time.Second)
+	for {
+		event, err := source.Read()
+		if err != nil {
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+		}
+		log.Printf("%s. %s %s\n", event.ID, event.Type, event.Data)
+	}
 }
